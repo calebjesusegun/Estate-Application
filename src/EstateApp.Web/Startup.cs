d421d47a4ec755c1f8a6493776986335dbfed801
calebjesusegun <jesuseguncaleb@gmail.com>
2020-06-07 16:05:22 +0100

    Added Application Db Context FINAL

--- src/EstateApp.Web/Startup.cs
+++ src/EstateApp.Web/Startup.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using EstateApp.Data.DatabaseContexts.ApplicationDbContext;
 using EstateApp.Data.DatabaseContexts.AuthenticationDbContext;
 using Microsoft.AspNetCore.Builder;
