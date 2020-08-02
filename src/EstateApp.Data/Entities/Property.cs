99e1d6108a534b4a94a0b5ea3b19ee41910fe096
calebjesusegun <jesuseguncaleb@gmail.com>
2020-08-02 23:56:34 +0100

    Added Property Column ALL

--- src/EstateApp.Data/Entities/Property.cs
+++ src/EstateApp.Data/Entities/Property.cs
@@ -4,5 +4,13 @@ namespace EstateApp.Data.Entities
    public class Property : BaseEntity
    {
       public string Title { get; set; } //BEDROOM FLAT IN OTA
+      public string ImageUrl { get; set; }
+      public double Price { get; set; }
+      public string Description { get; set; }
+      public int NumberOfRooms { get; set; }
+      public int NumberOfBaths { get; set; }
+      public int NumberOfToilets { get; set; }
+      public string Address { get; set; }
+      public string ContactPhoneNumber { get; set; }
    }
 }
